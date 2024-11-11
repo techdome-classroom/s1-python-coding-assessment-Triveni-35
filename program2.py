0f780643e588d7e53e97a27563e5dab4c51ee212
Triveni-35 <triveni45450@gmail.com>
2024-11-11 13:19:16 +0530

    Implemented island counting and pattern matching solutions

--- program2.py
+++ program2.py
@@ -1,5 +1,11 @@
 def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
-        return False
+        def dfs(i, j):
+                if j == len(p):
+                        return i == len(s)
+                if p[j] == '*':
+                        return dfs(i, j+1) or (i < len(s) and dfs(i+1, j))
+                if i < len(s) and (p[j] == '?' or s[i] == p[j]):
+                        return dfs(i+1, j+1)
+                return False
+        
+        return dfs(0, 0)
